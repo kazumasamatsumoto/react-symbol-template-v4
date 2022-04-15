d828c2165341821f3a100718053969b6eb0fecef
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2022-04-15 12:54:22 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Account, NetworkType } from "symbol-sdk";
 
 function App() {
+  const account = Account.generateNewAccount(NetworkType.TEST_NET);
+  console.log(
+    "Your new account address is:",
+    account.address.pretty(),
+    "and its private key",
+    account.privateKey
+  );
   return (
     <div className="App">
       <header className="App-header">
